7bd007f1c0a20f48cb182e891a17f1c7b0be1059
Emerson Guerrero <emersonguerrero.fr@academia.umb.edu.co>
2024-11-01 14:31:35 -0500

    add product card fixed design

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -24,10 +24,10 @@ const {user} = useContext(UserContext);
       <DataStatsOne />
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
-        <ChartOne />
+        {/* <ChartOne />
         <ChartTwo />
         <ChartThree />
-        <MapOne />
+        <MapOne /> */}
         <div className="col-span-12 xl:col-span-8">
           <TableOne />
         </div>
